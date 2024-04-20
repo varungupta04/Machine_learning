cbad409fd7d7abe3fe88e531f130a08e5a14c12c
Varun <>
2024-04-20 10:45:46 +0530

    ipynb for deployment

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='Krish',
+author='Varun',
 author_email='varungupta004@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
